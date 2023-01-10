0ab87a2c6b478747e50f7549c260b65ba59859a7
marco mejia <tomatin77@gmail.com>
2023-01-09 21:45:18 -0600

    carpeta public

--- APINODE/server.js
+++ APINODE/server.js
@@ -2,16 +2,31 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const router = express.Router();
+const {success,error} = require('./module/response')
+
+
 let app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(router)
 
 router.get('/message',(req,res)=>{
-    console.log(req.body);
-    res.send('Hola maquinola')
+    console.log(req.headers);
+    res.header({
+        "custom-header":"Valor Personalizado"
+    })
+    
+    success(req, res,"una respuesta chida")
 })
+
+
 router.post('/message',(req,res)=>{
-    res.send('Hola maquinola desde message')
+
+    if(req.query.error == "ok"){
+        error(req,res,'Error Simulado', 400)
+    }
+    /* res.send('Hola maquinola desde message') */
+    success(req,res, "Creado Correctamente")
 })
 
 
@@ -19,5 +34,7 @@ router.post('/message',(req,res)=>{
 app.use('/',(req,res)=>{
     res.send('hola maquinila')
 }) */
+
+app.use('/app', express.static('public'))
 app.listen(3000)
 console.log('la aplicacion esta funcionando en el puerto 3000');
